f3fca3c2e22edfa084efdc5bf7a753d1b90c8e4c
ptrvandenberg <ptr.vandenberg@gmail.com>
2017-03-14 22:00:55 +1100

    Update VIPER_model.py

--- VIPER_model.py
+++ VIPER_model.py
@@ -33,7 +33,7 @@ def validate_input(dat):
         rtn['Test 0 - Setting / Weeks'] = 'value <= 0'
     elif dat.parse('settings')['nbr_roster_weeks']['value'] % 1 <> 0:
         rtn["Test 0 - Setting / Weeks"] = 'value not integer'
-    else
+    else:
         print('Test 0 - Setting / Weeks: Passed')
     
     return rtn
