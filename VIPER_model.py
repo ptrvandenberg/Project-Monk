f4476fa9966afca32642e31b16ba4ac681c42f53
ptrvandenberg <ptr.vandenberg@gmail.com>
2017-04-01 20:58:39 +1100

    Update VIPER_model.py

--- VIPER_model.py
+++ VIPER_model.py
@@ -45,7 +45,7 @@ def solve(dat):
     restricted = dat.parse('restricted')
     restricted = restricted.set_index('memid','dayseq','shiftcd')
     
-    # [001] Commence model definition and set optimisation direction
+    # [001] Commence model definition and set optimisation direction.
     model = LpProblem("roster", LpMaximize)
     
     # Create and define the problem variables
@@ -60,12 +60,12 @@ def solve(dat):
     RN_bin8 = LpVariable.dicts("NG_bin8_%s", members.index, 0, 1, LpBinary)
     eor = LpVariable.dicts("eor_%s_%s", (members.index, days.index), 0, 1, LpBinary)
     
-    # [001] Set the objective
+    # [001] Set the objective.
     model += lpSum([x[m][d][s] for m in members.index for d in days.index for s in shifts.index])
     
     # STRUCTURAL CONSTRAINTS
 
-    # [002] Each member on each day has to be assigned to one and only one shift, including part-time, rest and leave shifts
+    # [002] Each member on each day has to be assigned to one and only one shift, including part-time, rest and leave shifts.
     for m in members.index:
         for d in days.index:
             model += lpSum([x[m][d][s] for s in shifts.index]) == 1
@@ -80,19 +80,19 @@ def solve(dat):
     
     # COMPLEX CONSTRAINTS
     
-    # [003] Each member needs to be assigned to 5*FTE*weeks -/+ carryover rests shifts, excluding part-time and rest shift
+    # [003] Each member needs to be assigned to 5*FTE*weeks -/+ carryover rests shifts, excluding part-time and rest shift.
     for m in members.index:
         model += lpSum([x[m][d][s] for d in days.index for s in shifts.index if s <> "XP" and s <> "XR"]) == settings.ix['nbr_roster_weeks','value'] * 5 * members.ix[m,'fte'] - carryover.ix[m,'r0_rests'] + r2_rests[m]
     
-    # [004] Each member needs to be assigned 2*weeks +/- carryover rest shifts
+    # [004] Each member needs to be assigned 2*weeks +/- carryover rest shifts.
     for m in members.index:
         model += lpSum([x[m][d]["XR"] for d in days.index]) == settings.ix['nbr_roster_weeks','value'] * 2 + carryover.ix[m,'r0_rests'] - r2_rests[m]
     
-    # [005] Each member needs to be assigned to 5*(1-FTE)*weeks part-time shifts
+    # [005] Each member needs to be assigned to 5*(1-FTE)*weeks part-time shifts.
     for m in members.index:
         model += lpSum([x[m][d]["XP"] for d in days.index]) == settings.ix['nbr_roster_weeks','value'] * 5 * (1 - members.ix[m,'fte'])
     
-    # [006] Each member can carryover up to 2 rests if he/she is on 7 consecutive night shifts in the current roster; 0 if less
+    # [006] Each member can carryover up to 2 rests if he/she is on 7 consecutive night shifts in the current roster; 0 if less.
     for m in members.index:
         model += r2_rests[m] <= 2 * (RN_bin1[m] + RN_bin4[m] + RN_bin5[m] + RN_bin8[m])
         model += RN_bin1[m] <= lpSum([x[m][d]["RN"] for d in range(1,8)]) / 7
@@ -104,7 +104,7 @@ def solve(dat):
         model += RN_bin8[m] <= lpSum([x[m][d]["RN"] for d in range(8,15)]) / 7
         model += RN_bin8[m] > lpSum([x[m][d]["RN"] for d in range(8,15)]) / 7 - 1
     
-    # [007] Each member is assigned one recovery shift following 4+ consecutive night shifts; 0 if less
+    # [007] Each member is eligble for one recovery shift following 4+ consecutive night shifts; ineligible if not.
     for m in members.index:
         if carryover.ix[m,'w0_nights'] == 7:
             model += eor[m][1] == 1
@@ -145,6 +145,15 @@ def solve(dat):
 
             model += eor[m][6+7*(w-1)] == 0
             model += eor[m][7+7*(w-1)] == 0
+
+    # [008] Each member is assigned one recovery shift if they are eligible and work the following day; 0 if not.
+    
+    for m in members.index:
+        for d in days.index:
+            if d <> settings.ix['nbr_roster_weeks','value'] * 7:
+                model += x[m][d]["OR"] <= eor[m][d]
+                model += x[m][d]["OR"] <= lpSum([x[m][d+1][s] for s in shifts.index if s not in ("XL","XP","XR")])
+                model += x[m][d]["OR"] > eor[m,d] + lpSum([x[m][d+1][s] for s in shifts.index if s not in ("XL","XP","XR")]) - 2
 
     # COMPOUNDED CONSTRAINTS
     
