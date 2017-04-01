7b1400940d37c0fb106530df2b77b293c710c911
ptrvandenberg <ptr.vandenberg@gmail.com>
2017-04-01 21:43:16 +1100

    Update VIPER_model.py

--- VIPER_model.py
+++ VIPER_model.py
@@ -97,7 +97,7 @@ def solve(dat):
             if d == 1:
                 model += lpSum([x[m][d][s] * shifts.ix[s,'starttime'] for s in shifts.index]) >=  shifts.ix[carryover.ix[m,'d0_shift'],'endtime'] + 10 - 24
             else:
-                model += lpSum([x[m][d-1][s] * shifts.ix[s,'starttime'] for s in shifts.index]) + 10 - 24 <= lpSum([x[m][d][s] * shifts.ix[s,'starttime'] for s in shifts.index])
+                model += lpSum([x[m][d][s] * shifts.ix[s,'starttime'] for s in shifts.index]) >= lpSum([x[m][d-1][s] * shifts.ix[s,'starttime'] for s in shifts.index]) + 10 - 24
 
     # [007] Each member can carryover up to 2 rests if he/she is on 7 consecutive night shifts in the current roster; 0 if less.
     for m in members.index:
