f752587c1f6195e096599110ed9c9276b5304bf0
ptrvandenberg <ptr.vandenberg@gmail.com>
2017-05-08 20:56:13 +1000

    Update ViPER_model.py

--- ViPER_model.py
+++ ViPER_model.py
@@ -164,9 +164,9 @@ def solve(dat):
     if rules.ix[40, unit] == 'Yes':
         for m in members.index:
             if carryover.ix[m,'r0_rests'] >= 4:
-                co_rest == 0
+                co_rest = 0
             else:
-                co_rest == 4 - carryover.ix[m,'r0_rests']
+                co_rest = 4 - carryover.ix[m,'r0_rests']
             model += lpSum([x[m][d]["XR"] for d in days]) == weeks * 2 + co_rest - r2_rests[m]
     
     # [0050] PART-TIME – Each member needs to be assigned to 5*(1-FTE)*weeks part-time shifts.
