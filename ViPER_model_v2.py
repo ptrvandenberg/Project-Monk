c66cdaca7197a5f14f0f8554c41e53764743e2e7
ptrvandenberg <ptr.vandenberg@gmail.com>
2017-05-24 21:49:00 +1000

    Update ViPER_model_v2.py

--- ViPER_model_v2.py
+++ ViPER_model_v2.py
@@ -119,18 +119,18 @@ def solve(dat):
 
     predetermined = predetermined.set_index(['member_id','week'])
 
-#    for m in members.index:        
-#        if members.ix[m,'longshifts'] >= 1:
-#            ls = carryover.ix[m,'w0_longshift']
-#            for w in range(1,weeks+1):
-#                if ls < members.ix[m,'longshifts']:
-#                    ls += 1
-#                else:
-#                    ls = 1
-#                for d in range(1,7+1):
-#                    if isnull(predetermined.ix[m].ix[w,d-1]):
-#                        if not isnull(longshifts.ix[m].ix[ls,d-1]):
-#                            predetermined.ix[m].ix[w,d-1] = longshifts.ix[m].ix[ls,d-1]
+    for m in members.index:        
+        if members.ix[m,'longshifts'] >= 1:
+            ls = carryover.ix[m,'w0_longshift']
+            for w in range(1,weeks+1):
+                if ls < members.ix[m,'longshifts']:
+                    ls += 1
+                else:
+                    ls = 1
+                for d in range(1,7+1):
+                    if isnull(predetermined.ix[m].ix[w,d-1]):
+                        if not isnull(longshifts.ix[m].ix[ls,d-1]):
+                            predetermined.ix[m].ix[w,d-1] = longshifts.ix[m].ix[ls,d-1]
 
     # TO BE DELETED
     
