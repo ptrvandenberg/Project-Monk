9720518e6ccd8150715fe3261bfb6c2a7d34eaac
ptrvandenberg <ptr.vandenberg@gmail.com>
2017-05-27 21:35:11 +1000

    Update ViPER_model_v2.py

--- ViPER_model_v2.py
+++ ViPER_model_v2.py
@@ -378,17 +378,13 @@ def solve(dat):
                 model += crew_am_bin1[d+7*(w-1)] + crew_am_bin2[d+7*(w-1)] + crew_am_bin3[d+7*(w-1)] == 1
                 model += crew_pm_bin1[d+7*(w-1)] + crew_pm_bin2[d+7*(w-1)] + crew_pm_bin3[d+7*(w-1)] == 1
                 for m in members.index:
-                    print m, members.ix[m,'crew']
                     if members.ix[m,'crew'] == 1:
-                        print m, members.ix[m,'crew'], 1
                         model += x[m][d+7*(w-1)]["RA1"] + x[m][d+7*(w-1)]["RA2"] + x[m][d+7*(w-1)]["RA3"] <= crew_am_bin1[d+7*(w-1)]
                         model += x[m][d+7*(w-1)]["RP1"] + x[m][d+7*(w-1)]["RP2"] <= crew_pm_bin1[d+7*(w-1)]
                     if members.ix[m,'crew'] == 2:
-                        print m, members.ix[m,'crew'], 2
                         model += x[m][d+7*(w-1)]["RA1"] + x[m][d+7*(w-1)]["RA2"] + x[m][d+7*(w-1)]["RA3"] <= crew_am_bin2[d+7*(w-1)]
                         model += x[m][d+7*(w-1)]["RP1"] + x[m][d+7*(w-1)]["RP2"] <= crew_pm_bin2[d+7*(w-1)]
                     if members.ix[m,'crew'] == 3:
-                        print m, members.ix[m,'crew'], 3
                         model += x[m][d+7*(w-1)]["RA1"] + x[m][d+7*(w-1)]["RA2"] + x[m][d+7*(w-1)]["RA3"] <= crew_am_bin3[d+7*(w-1)]
                         model += x[m][d+7*(w-1)]["RP1"] + x[m][d+7*(w-1)]["RP2"] <= crew_pm_bin3[d+7*(w-1)]
     
