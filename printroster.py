8be927c987e5ee6bad3bde65cee25188e0fc3b0c
ptrvandenberg <ptr.vandenberg@gmail.com>
2017-06-12 20:15:02 +1000

    Create printroster.py

---
+++ printroster.py
@@ -0,0 +1,11 @@
+# ViPER: Victoria Police Electronic Rostering
+# Copyright 2017, Peter van den Berg
+
+# Project Monk: SOCIT Fawkner
+# This file implements a function [cleanroster] that processes the printroster for use as an input into the solve function
+
+def cleanroster(rosterdat):
+
+  print rosterdat
+
+  return rosterdat
